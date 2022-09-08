378052e8ca3e7700d5068554dd13f259e239da40
”Artyom <SantaD3zert@gamil.com>
2022-09-08 15:10:10 +0300

    Added edit profile(avatar and fullname) and view all users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  root 'users#index' 
-  get 'users/index'
-  get 'users/profile', as: 'user_root'
+  root 'pages#home' 
+  get 'profile', action: :profile, controller: 'users'
+  scope :user do
+    resources :users 
+  end
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
-    omniauth_callbacks: 'users/omniauth_callbacks' }
-
+    omniauth_callbacks: 'users/omniauth_callbacks'}
 end
