7c8e2c0f312ec2ebdb8033d338cbfe27729b566f
”Artyom <SantaD3zert@gamil.com>
2022-09-09 12:00:15 +0300

    fix User, dosen-t work update google auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root 'pages#home' 
   get 'profile', action: :profile, controller: 'users'
-  scope :user do
-    resources :users 
+  scope :all do
+    resources :users, only: [:index, :show] 
   end
   devise_for :users, controllers: {
     registrations: 'users/registrations',
+    passwords: 'users/passwords',
     sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks'}
 end
