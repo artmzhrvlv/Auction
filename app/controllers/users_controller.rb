10fec905b230157e1bfd662fc6bef043eae8a16e
”Artyom <SantaD3zert@gamil.com>
2022-09-21 17:01:50 +0300

    Revert "Revert "Revert "fix User, dosen-t work update google auth"""
    This reverts commit 19576f0bebdca77386706d002cb859ab35d7e0f6.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,37 @@
 class UsersController < ApplicationController
 
-  before_action :authenticate_user!
-  
+  before_action :set_user, only: %i[profile update destroy edit]
+
   def index
     @users = User.all
   end
 
   def profile
-    @user = current_user
+
+  end
+
+  def update
+    @user.update( params.require(:user).permit(:full_name, :avatar_url))
+    redirect_to profile_path, notice: "profile updated"
+  end
+
+  def edit
+
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to root_path, notice: "User removed"
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def set_user
+    @user = current_user
   end
   
 end
